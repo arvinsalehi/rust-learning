15c04bc9b2127a95a43b266485166e3311b18dc2
arvin <83908258+arvinsalehi@users.noreply.github.com>
2024-06-16 20:02:27 +0000

    getting modular baby

---
+++ restaurant/src/front_of_house.rs
@@ -0,0 +1,19 @@
+
+pub mod hosting {
+    pub fn add_to_waitlist() {}
+
+    pub fn seat_at_table() {}
+}
+
+mod serving {
+    fn take_order() {}
+    fn server_order() {}
+    fn payment() {}
+    fn deliver_feedback() {}
+}
+
+mod security {
+    fn escort() {}
+    fn patrol() {}
+    fn eject() {}
+}
