ac3ed25f6f5dcaf1d2620c03879a570fedbf80b0
arvin <83908258+arvinsalehi@users.noreply.github.com>
2024-07-09 21:03:30 +0000

    Adding Alternate Solution

--- leet_code/longest_consecutive_seq/src/lib.rs
+++ leet_code/longest_consecutive_seq/src/lib.rs
@@ -27,3 +27,39 @@ impl Solution {
         max_length
     }
 }
+
+///
+/// Alternate style of Solution
+/// 
+// use std::collections::HashMap;
+// impl Solution {
+//     pub fn longest_consecutive(nums: Vec<i32>) -> i32 {
+//         if nums.is_empty() {
+//             return 0;
+//         }
+        
+//         let mut map: HashMap<i32, i32> = HashMap::new();
+//         for i in nums {
+//             map.entry(i).and_modify(|i| *i += 1).or_insert(1);
+//         }
+
+//         let mut max_len: i32 = 1;
+        
+//         for (key, val) in &map {
+//             match map.get(&(key-1)) {
+//                 Some(val) => continue,
+//                 None => {
+//                     let mut batch_len = 0;
+//                     let mut batch_key: i32 = *key;
+//                     while map.get(&batch_key) != None {
+//                            batch_len += 1;
+//                            batch_key += 1;
+//                     }
+//                     max_len = max_len.max(batch_len);
+//                 }
+//             }
+//         }
+//         max_len
+    
+//     }
+// }
